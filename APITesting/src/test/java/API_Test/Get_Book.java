59dc47e29ec517044b8b29a1068acd2f0a96b266
aman <caman6292@gmail.com>
2021-10-06 19:50:55 +0530

    my 2nd commit

--- APITesting/src/test/java/API_Test/Get_Book.java
+++ APITesting/src/test/java/API_Test/Get_Book.java
@@ -4,10 +4,13 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 public class Get_Book {
 	
+	private static final String[] getbook_details = null;
+	String isbn;
 	@Test
 	public void getbook_details()
 	{
@@ -19,6 +22,21 @@ public class Get_Book {
 		Assert.assertEquals(rs.getStatusCode(), 200);
 		
 		System.out.println(rs.body().prettyPrint());
+		
+		JsonPath js= new JsonPath(rs.asString());
+		   isbn= js.getString("books[4].isbn"); 
+		  //System.out.println(Long.parseLong(a));
+	}
+	
+	
+	@Test(dependsOnMethods = "getbook_details" )
+	public void postbookdetails() {
+		
+        RestAssured.baseURI="https://bookstore.toolsqa.com/BookStore/v1";
+		
+		Response rs=RestAssured.given().queryParam("ISBN", isbn).when().get("/Book").then().extract().response();
+		System.out.println(rs.getBody().asPrettyString());
+		
 	}
 
 }
